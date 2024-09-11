a45ca6ede7362a39ad414eeaf9b34bf1da743cc4
seedar <sidardosky3@gmail.com>
2024-09-11 04:26:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">first hello world in react js</h1>
+      <p>first paragraph in react</p>
+    </div>
+  );
+}
+
+export default App;
